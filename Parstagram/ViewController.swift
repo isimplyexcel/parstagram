774773fa3c1d123ad3fc9875a91c76d7b3bdc0f5
Paul Min <isimplyexcel@gmail.com>
2019-11-11 16:13:00 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Paul Min on 11/11/19.
+//  Copyright © 2019 Paul Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
